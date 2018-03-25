26276981e2cfa6cea5040a34b175cf8eafe1ccdc
Kaustubh <kaustubh.walokar@gmail.com>
2018-03-24 17:29:27 -0700

    added avl

--- probs/avl/avl.go
+++ probs/avl/avl.go
@@ -1,41 +1,85 @@
 package main
 
-import ("fmt"
-		"github.com/kaustubh-walokar/go/node")
+import ("fmt")
 
-
-func newNode(data int) (* node.Node){
-	return node.NewNode(data)
+type AvlNode struct {
+	Left *AvlNode
+	Right *AvlNode
+	Height int
+	Data int
 }
 
-func insert(data int, t *node.Node) (*node.Node){
+func (n *AvlNode) Print() (string){
+	return Print(n)
+}
+
+func Print(root *AvlNode) (string) {
+	
+	if(root == nil) {
+		return "."
+	} 
+		
+	l := Print(root.Left)
+	r := Print(root.Right)
+
+	return fmt.Sprintf("{%v(%v,%v)%v}",l ,root.Data, root.Height, r) 
+}
+
+func newNode(data int) (* AvlNode){
+	return &AvlNode{nil, nil, 0, data}
+}
+
+func insert(data int, t *AvlNode) (*AvlNode){
 	if (t == nil) {
 		return newNode(data)
 	}
-
-	if(data < t.Data.(int) ) {
-		if  (t.Left ==nil) {
-			t.Left = newNode(data)
-			return t.Left
-		} else {
-			return insert(data, t.Left)
-		}
-
+	
+	if(data < t.Data ) {
+		t.Left = insert(data, t.Left)
 	}
 
-	if(data > t.Data.(int) ) {
-		if  (t.Right ==nil) {
-			t.Right = newNode(data)
-			return t.Right
-		} else {
-			return insert(data, t.Right)
-		}
-
+	if(data > t.Data ) {
+		t.Right = insert(data, t.Right)
 	}
 
+	var l,r = 0,0
+	if t.Left != nil {
+		l= t.Left.Height
+	}
 
-	return t
+	if t.Right != nil {
+		r= t.Right.Height
+	}
+
+	if(l<r){
+		t.Height = r +1 
+	} else {
+		t.Height = l +1 
+	}
+
+	return t;
 }
+
+// func leftRotate(t *node.Node, parent *node.Node) (*node.Node){
+	
+// 	isRight := false;
+
+// 	if(parent.Right == t) {
+// 		isRight = true;
+// 	}
+
+// 	tmp := t.Right
+// 	t.Right = tmp.Left;
+// 	tmp.Left = t
+
+// 	if(isRight) {
+// 		parent.Right = tmp;
+// 	} else {
+// 		parent.Left = tmp;
+// 	}
+
+// 	return parent;
+// }
 
 func main() {
 	
